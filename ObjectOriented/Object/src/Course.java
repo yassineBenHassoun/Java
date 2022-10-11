608d07c89c5c05b95af8f51e9629612bcd98b93f
BenHassoun <y.benhassoun@gmail.com>
2022-10-11 12:18:53 +0200

    add exercices

--- ObjectOriented/Object/src/Course.java
+++ ObjectOriented/Object/src/Course.java
@@ -1,21 +1,183 @@
 /*******************************************
  * Completez le programme a partir d'ici.
  *******************************************/
+import java.util.ArrayList;
 
- class Vehicule {
-     private String name;
-     private double vitesse;
-     private int poids; 
-     private int carburant;
-     
-     public Vehicule (String name , double speed, int poids , int carburant) 
-     {
-         this.name = name;
-         this.vitesse = speed;
-         
+class Vehicule {
+   private String nom = "Anonyme";
+   private double vitesse = 130;
+   private int poids =  1000;
+   private int carburant = 0;
 
-     }
- }
+   public boolean estDeuxRoues(){
+		return false;
+	};
+
+   public String getNom() {
+       return this.nom;
+   }
+
+   public double getVitesse() {
+       return this.vitesse;
+   }
+
+   public int getPoids() {
+       return this.poids;
+   }
+
+   public int getCarburant() {
+       return this.carburant;
+   }
+
+  public Vehicule (String nom , double speed, int poids , int carburant) {
+        this.nom = nom;
+        this.vitesse = speed;
+        this.poids = poids;
+        this.carburant = carburant;
+  }
+
+  public Vehicule (double speed, int poids , int carburant) {
+       this.vitesse = speed;
+       this.poids = poids;
+       this.carburant = carburant;
+    }
+    public Vehicule ()
+    {
+        super();
+    }
+   public String toString()
+   {
+     return nom + " -> vitesse max = " + vitesse + " km/h, poids = " + poids + " kg";
+   }
+   public boolean  meilleur (Vehicule autreVehicule) {
+
+       double calculateBasicPerf = performance(this.vitesse, this.poids);
+       double calculateAutrePerf = performance(autreVehicule.vitesse, autreVehicule.poids);
+
+       if(calculateBasicPerf >= calculateAutrePerf) {
+           return true;
+       }
+       return false;
+   }
+
+   private double performance ( double vitesse , int poids)  {
+       return vitesse / poids;
+   }
+
+   public void deduireCarburant(int tours) {
+		carburant -= tours;
+		if (carburant < 0){
+			carburant = 0;
+		}
+	}
+}
+
+class Voiture extends Vehicule
+{
+   private String category;
+
+   public Voiture (String name , double speed, int poids , int carburant, String category)
+   {
+        super(name,speed,poids, carburant);
+        this.category = category;
+   }
+
+   public String getCategorie() {
+       return this.category;
+   }
+
+   public void setCategory(String category) {
+       this.category = category;
+   }
+
+    public String toString()
+    {
+        return super.toString() + ", Voiture de " + category;
+    }
+
+}
+class Moto extends Vehicule
+{
+   private boolean sidecar = false;
+
+   public Moto (String name , double speed, int poids , int carburant,boolean sidecar)
+   {
+       super(name,speed,poids, carburant );
+       this.sidecar = sidecar;
+   }
+
+   public Moto (String name , double speed, int poids , int carburant)
+   {
+       super(name,speed,poids, carburant );
+   }
+
+   public String toString()
+    {
+        return super.toString() + ", Moto" 
+        + (sidecar == true ? ", avec sidecar" : "");
+    }
+
+	public boolean estDeuxRoues(){
+		return !sidecar;
+	};
+
+}
+
+abstract class Rallye{
+ 	  public abstract boolean check();
+}
+
+class GrandPrix  extends Rallye
+{
+   private ArrayList<Vehicule> vehicules;
+
+   public GrandPrix ()
+   {
+       vehicules = new ArrayList<Vehicule>();
+   }
+
+   public void ajouter(Vehicule vehicule)
+   {
+       vehicules.add(vehicule);
+   }
+
+   @Override
+	   public boolean check() {
+ 		boolean deuxRoues = false;
+ 		boolean pasDeuxRoues = false;
+ 		for (Vehicule vehicule: vehicules){
+ 			if (vehicule.estDeuxRoues()){
+ 				deuxRoues = true;
+ 			}
+ 			else{
+ 				pasDeuxRoues = true;
+ 			}
+ 		}
+ 		return deuxRoues != pasDeuxRoues;
+ 	}
+
+	public void run(int tours) {
+		if (check()){
+			Vehicule winner = null;
+			for (Vehicule vehicule: vehicules){
+				vehicule.deduireCarburant(tours);
+				if (winner == null && vehicule.getCarburant() > 0
+						|| winner != null && vehicule.getCarburant() > 0 && vehicule.meilleur(winner)){
+                           winner = vehicule;
+				}
+			}
+			if (winner != null){
+				System.out.println("Le gagnant du grand prix est -> " + winner);
+			}
+			else {
+				System.out.println("Elimination de tous les vehicules");
+			}
+		}
+		else {
+			System.out.println("Pas de Grand Prix");
+		}
+	}
+}
 /*******************************************
  * Ne pas modifier apres cette ligne
  * pour pr'eserver les fonctionnalit'es et
@@ -35,7 +197,9 @@ public class Course {
         // les arguments du constructeurs sont dans l'ordre:
         // le nom, la vitesse, le poids, le carburant
         Vehicule v1 = new Vehicule("Ferrari", 300.0, 800, 30);
+        //0.3
         Vehicule v2 = new Vehicule("Renault Clio", 180.0, 1000, 20);
+        //
         System.out.println();
         System.out.println(v1);
         System.out.println();
@@ -48,7 +212,7 @@ public class Course {
         }
         // FIN PARTIE 1
 
-        // PARTIE2
+       // PARTIE2
         System.out.println();
         System.out.println("Test partie 2 : ");
         System.out.println("----------------");
@@ -101,6 +265,7 @@ public class Course {
         gp4.ajouter(m2);
         System.out.println(gp4.check());
         // FIN PARTIE 3
+
 
         // PARTIE 4
         System.out.println();
